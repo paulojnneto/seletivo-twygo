a0f44408eee387aab3f173107976c016bf94948f
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2025-08-05 20:37:21 -0300

    feat: hello world

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Button, HStack } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <HStack>
+      <Button variant="outline" colorPalette='teal'>hello</Button>
+      <Button variant="outline" colorPalette='teal'>world</Button>
+    </HStack>
+  )
+}
+
+export default App
