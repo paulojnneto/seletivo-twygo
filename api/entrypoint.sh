083bc3a314b92905d4655995033a9a08516eeb9d
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2025-08-08 16:20:29 -0300

    fix: please work

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Running migrations..."
+echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
-echo "Starting server..."
-exec node dist/index.js
+echo "Starting the app..."
+exec "$@"
